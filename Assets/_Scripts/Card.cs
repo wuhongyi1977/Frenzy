9fea9261eb777e97ff57b5394791a4880278e2fa
Darkchicken <joshshucker@hotmail.com>
2016-05-24 23:24:53 -0700

    Created some preliminary scripts
    Created a script to handle the player's deck
    Created a script to handle a card object

---
+++ Assets/_Scripts/Card.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// A script to handle the functions that should be available to every
+/// card in the game.
+/// </summary>
+public class Card : MonoBehaviour {
+
+    //the amount of time a card takes after being played to become active
+    float castTime;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
