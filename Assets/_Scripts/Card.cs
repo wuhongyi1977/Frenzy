c04848907fbbb09dd47606619f49adc97d70fc56
ken_miller <ken_miller521@yahoo.com>
2016-05-25 11:47:26 -0700

    MVP Phase 1
    -Made initial game objects
    -Turned Card.cs into an abstract script. Any card inheriting from this
    must implement Start() and Update(). Also includes the logic for
    dragging the card.
    -Made DamageCard.cs this script could be the base for any card that
    damages in the future(Creature/Spell damage cards)
    -Made game mangers. Copy/pasted code from PlayerDeck into Player1Manager
    since the manager will handle all of that. No logic for master game
    manager.
    -Added Table layer to sorting layers

--- Assets/_Scripts/Card.cs
+++ Assets/_Scripts/Card.cs
@@ -5,19 +5,32 @@ using System.Collections;
 /// A script to handle the functions that should be available to every
 /// card in the game.
 /// </summary>
-public class Card : MonoBehaviour {
-
+public abstract class Card : MonoBehaviour {
+	private Vector3 screenPoint;
+	private Vector3 offset;
     //the amount of time a card takes after being played to become active
-    float castTime;
+   //float castTime;
 
 	// Use this for initialization
-	void Start ()
-    {
-	
+	public abstract void Start ();				//Abstract method for start
+	public abstract void Update ();				//Abstract method for Update
+
+	//Registers that the player has clicked on the card
+	public void OnMouseDown()			
+	{
+		screenPoint = Camera.main.WorldToScreenPoint (gameObject.transform.position);
+		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	//Registers that the player has let go of the card
+	public  void OnMouseUp()			
+	{
+		gameObject.transform.position = new Vector3 (0, -3.79f, 0);
+	}
+	//Registers that the card is being dragged
+	public  void OnMouseDrag()			
+	{
+		Vector3 curScreenPoint = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+		Vector3 curPosition = Camera.main.ScreenToWorldPoint (curScreenPoint) + offset;
+		transform.position = curPosition;
 	}
 }
---
+++ Assets/_Scripts/DamageCard.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageCard : Card
+{
+	private float currentTime;
+	private Vector3 graveyardPosition;
+	private bool inGraveyard;
+	private bool inSummonZone;
+	private GameObject gameManager;
+	public float castTime;
+	public int damageToDeal;
+
+	public override void Start()
+	{
+		//Debug.Log ("DAMAGE CARD START");
+		currentTime = 0;
+		//gameManager = GameObject.Find ("GameManager");
+		//graveyardPosition = GameObject.Find ("Player1Graveyard").transform.position;
+	}
+
+	public override void Update()
+	{
+		//Debug.Log ("DAMAGE CARD UPDATE");
+		if (!inGraveyard && inSummonZone) {
+			currentTime += Time.deltaTime;
+			Debug.Log (currentTime);
+			if (currentTime > castTime) {
+				//gameManager.GetComponent<GameManager> ().attackEnemy (damageToDeal);
+				gameObject.transform.position = graveyardPosition;
+				currentTime = 0;
+				inGraveyard = true;
+			}
+		}
+	}
+}
