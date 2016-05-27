63b95cdc3542b8736f7f64e7ab11c3b31e0d6f64
Darkchicken <joshshucker@hotmail.com>
2016-05-26 22:24:35 -0700

    Beginning work on playfab scripts
    Created folder for playfab scripts
    Created playfab login script to handle login
    Created playfab data store script to hold data retrieved from playfab

---
+++ Assets/_Scripts/Playfab Scripts/PlayFabDataStore.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// This script stores data that is retieved from playfab
+/// </summary>
+public class PlayFabDataStore : MonoBehaviour
+{
+
+    public static string sessionTicket;
+    public static string playFabId;
+    public static string userName;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/_Scripts/Playfab Scripts/PlayfabLogin.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using PlayFab;
+using PlayFab.ClientModels;
+
+public class PlayfabLogin : MonoBehaviour
+{
+    //input field where the user inputs their email address
+    public InputField loginUsernameField;
+    //input field where the user inputs their password
+    public InputField loginPasswordField;
+
+    void Awake()
+    {
+        PlayFabSettings.TitleId = "57F4";
+       
+    }
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    //Login to playfab/game
+    public static void PlayFabLogin(string username, string password)
+    {
+        var request = new LoginWithPlayFabRequest()
+        {
+            TitleId = PlayFabSettings.TitleId,
+            Username = username,
+            Password = password
+        };
+
+        PlayFabClientAPI.LoginWithPlayFab(request, (result) =>
+        {
+            Debug.Log("Login Successful!");
+          
+            PlayFabDataStore.playFabId = result.PlayFabId;
+            PlayFabDataStore.userName = username;
+            PlayFabDataStore.sessionTicket = result.SessionTicket;
+           
+            GetPhotonToken();
+        }, (error) =>
+        {
+            Debug.Log("Login Failed!");
+            Debug.Log(error.ErrorMessage.ToString());
+        });
+    }
+
+    //Get Photon Token from playfab
+    public static void GetPhotonToken()
+    {
+        /*
+        var request = new GetPhotonAuthenticationTokenRequest();
+        {
+            request.PhotonApplicationId = "ee6881e0-06d0-4b7c-b552-a4087cd14926".Trim();
+        }
+
+        PlayFabClientAPI.GetPhotonAuthenticationToken(request, (result) =>
+        {
+            string photonToken = result.PhotonCustomAuthenticationToken;
+            Debug.Log(string.Format("Yay, logged in in session token: {0}", photonToken));
+            PhotonNetwork.AuthValues = new AuthenticationValues();
+            PhotonNetwork.AuthValues.AuthType = CustomAuthenticationType.Custom;
+            PhotonNetwork.AuthValues.AddAuthParameter("username", PlayFabDataStore.playFabId);
+            PhotonNetwork.AuthValues.AddAuthParameter("Token", result.PhotonCustomAuthenticationToken);
+            PhotonNetwork.AuthValues.UserId = PlayFabDataStore.playFabId;
+            PhotonNetwork.ConnectUsingSettings("1.0");
+            //make sure all players are synced to same scene in same room
+            PhotonNetwork.automaticallySyncScene = true;
+            GetUserStatistic();
+        }, (error) =>
+        {
+            Debug.Log("Photon Connection Failed! " + error.ErrorMessage.ToString());
+        });
+        */
+    }
+}
