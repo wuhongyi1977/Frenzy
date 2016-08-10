219ed0f501e906bed1f7cc3a2bfa7f5b05e92d23
Darkchicken <joshshucker@hotmail.com>
2016-08-09 19:43:43 -0700

    More work on direct damage
    Added bools to check if target has been selected
    Added line component to one shot card (to display target)

--- Assets/_Scripts/OneShotHealth.cs
+++ Assets/_Scripts/OneShotHealth.cs
@@ -9,6 +9,12 @@ public class OneShotHealth : Card
 
     public bool isSelectable = true;
 
+    //checks to see if the card is ready for a target to be selected
+    private bool waitingForTarget = false;
+    //checks to see if the card has been given a target yet
+    private bool targetSelected = false;
+
+
     public override void Start()                //Abstract method for start
     {
         //set card data variables
@@ -43,37 +49,45 @@ public class OneShotHealth : Card
         //If the card is Not in the graveyard and is in the summon zone
         if (!inGraveyard && inSummonZone)
         {
-
-            //Increment the current Time
-            currentTime -= Time.deltaTime;
-            if (summonZoneTextBox == null)
-            {
-                //if this is the local object
-                if (photonView.isMine)
-                {
-                    summonZoneTextBox = localPlayer.GetComponent<PlayerController>().getSummonZone(gameObject);
-                }
-                else //if this is the network copy
-                {
-                    summonZoneTextBox = networkOpponent.GetComponent<PlayerController>().getSummonZone(gameObject);
-                }
-            }
-            else
-            {
-                summonZoneTextBox.text = currentTime.ToString("F1");
-            }
-
-            //cardTimerBox.text = currentTime.ToString ("F1");
             //IF the current time is larger than or equal to the cast time
             isDraggable = false;
-            if (currentTime <= 0)
+            //if the card is in the summon zone and isnt waiting for a target yet, make it true
+            if (waitingForTarget == false)
             {
-                //reset the timer
-                currentTime = 0;
-                //Set state of card to being in the graveyard
-                inGraveyard = true;
-                //Set state of card to not being in the summon zone
-                inSummonZone = false;
+                waitingForTarget = true;
+            }
+            //if the target has been selected, begin countdown
+            if (targetSelected == true)
+            {
+
+                //Increment the current Time
+                currentTime -= Time.deltaTime;
+                if (summonZoneTextBox == null)
+                {
+                    //if this is the local object
+                    if (photonView.isMine)
+                    {
+                        summonZoneTextBox = localPlayer.GetComponent<PlayerController>().getSummonZone(gameObject);
+                    }
+                    else //if this is the network copy
+                    {
+                        summonZoneTextBox = networkOpponent.GetComponent<PlayerController>().getSummonZone(gameObject);
+                    }
+                }
+                else
+                {
+                    summonZoneTextBox.text = currentTime.ToString("F1");
+                }
+ 
+                if (currentTime <= 0)
+                {
+                    //reset the timer
+                    currentTime = 0;
+                    //Set state of card to being in the graveyard
+                    inGraveyard = true;
+                    //Set state of card to not being in the summon zone
+                    inSummonZone = false;
+                }
             }
 
         }
@@ -103,6 +117,101 @@ public class OneShotHealth : Card
         }
     }
     /*
+    public override void OnMouseOver()
+    {
+        
+        if (waitingForTarget == true && targetSelected == false)//(creatureCanAttack)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                localPlayer.GetComponent<PlayerController>().drawLineOn();
+
+            }
+
+        }
+        
+
+        if (photonView.isMine)
+        {
+            localPlayer.GetComponent<PlayerController>().setMousedOverCard(gameObject);
+        }
+        else
+        {
+            networkOpponent.GetComponent<PlayerController>().setMousedOverCard(gameObject);
+        }
+        
+        if (waitingForTarget == true && targetSelected == false)//(creatureCanAttack)
+        {
+            if (photonView.isMine)
+            {
+                localPlayer.GetComponent<PlayerController>().drawLineOn();
+            }
+        }
+    }
+
+    public override void OnMouseUp()
+    {
+        if (isSelectable == true)
+        {
+            localPlayer.GetComponent<PlayerController>().makeLineInvisible();
+            localPlayer.GetComponent<PlayerController>().drawLineOff();
+
+            dropped = true;
+
+            
+            //if this is the local card object
+            if (photonView.isMine)
+            {
+
+                if (target == "Creature" || target == "All")//(creatureCanAttack)
+                {
+                    networkOpponent.GetComponent<PlayerController>().ChangeHealth(opponentHealthChange);
+                    localPlayer.GetComponent<PlayerController>().creatureCardIsDropped(gameObject, cardHandPos);
+
+                }
+                else
+                {
+                    localPlayer.GetComponent<PlayerController>().cardIsDropped(gameObject, cardHandPos);
+
+                }
+            }
+            else
+            {
+                if (target == "Creature" || target == "All")//(creatureCanAttack)
+                {
+
+                    networkOpponent.GetComponent<PlayerController>().creatureCardIsDropped(gameObject, cardHandPos);
+
+                }
+                else
+                {
+                    networkOpponent.GetComponent<PlayerController>().cardIsDropped(gameObject, cardHandPos);
+                }        
+            }
+            
+            //finds the text box that corresponds to the summon zone
+            if (summonZoneTextBox == null)
+            {
+                //if this is the local card object
+                if (photonView.isMine)
+                {
+                    summonZoneTextBox = localPlayer.GetComponent<PlayerController>().getSummonZone(gameObject);
+
+                }
+                else
+                {
+                    summonZoneTextBox = networkOpponent.GetComponent<PlayerController>().getSummonZone(gameObject);
+
+                }
+
+            }
+            
+        }
+        
+
+    }
+    */
+    /*
     //ADDED TO ATTEMPT TARGETED DAMAGE
     public override void OnMouseDown()
     {
@@ -119,9 +228,7 @@ public class OneShotHealth : Card
         {
             localPlayer.GetComponent<PlayerController>().makeLineInvisible();
             localPlayer.GetComponent<PlayerController>().drawLineOff();
-            //TEST
-            //p1Manager.GetComponent<Player1Manager> ().makeLineInvisible ();
-            //p1Manager.GetComponent<Player1Manager> ().drawLineOff ();
+           
             dropped = true;
             //if this is the local card object
             if (photonView.isMine)
@@ -174,36 +281,8 @@ public class OneShotHealth : Card
         }
 
     }
-    public override void OnMouseOver()
-    {
-        if (target == "Creature" || target == "All")//(creatureCanAttack)
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
-                localPlayer.GetComponent<PlayerController>().drawLineOn();
-              
-            }
-
-        }
-       
-        if (photonView.isMine)
-        {
-            localPlayer.GetComponent<PlayerController>().setMousedOverCard(gameObject);
-            
-        }
-        else
-        {
-            networkOpponent.GetComponent<PlayerController>().setMousedOverCard(gameObject);
-            
-        }
-        if (target == "Creature" || target == "All")//(creatureCanAttack)
-        {
-            if (photonView.isMine)
-            {
-                localPlayer.GetComponent<PlayerController>().drawLineOn();
-              
-            }
-        }
-    }
     */
+
+
+
 }
