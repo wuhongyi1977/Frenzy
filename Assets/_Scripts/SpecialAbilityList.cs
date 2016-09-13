fec2cac441858fcfc44c9cc170c05bf75b498c16
Darkchicken <joshshucker@hotmail.com>
2016-09-12 18:42:57 -0700

    Added Ability list script
    Eventually, each card function can be placed in this script and called
    with info from playfab

---
+++ Assets/_Scripts/SpecialAbilityList.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpecialAbilityList : MonoBehaviour
+{
+    //seal of ice,
+    public void SetTargetInactive(GameObject target, float seconds)
+    {
+        
+    }
+	
+}
